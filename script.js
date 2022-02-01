773da047a42a2da000db6c41a66d115eb81ff83b
deep7raja <56893558+deep7raja@users.noreply.github.com>
2022-02-01 10:44:27 +0530

    functions added to add new books to table and displaying on html

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, numPages, isRead){
     this.title = title;
     this.author = author;
@@ -14,3 +16,38 @@ const book1 = new Book('University', 'zimansky', 5627, true);
 const book2 = new Book('Physics', 'LRT', 69420, false);
 console.log(book1.info());
 console.log(book2.info());
+
+
+
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function makeTableData(content){
+  let td = document.createElement('td');
+  td.textContent = content;
+  return td;
+}
+function makeRow(book){
+  let tr = document.createElement('tr');
+  tr.appendChild(makeTableData(book.title));
+  tr.appendChild(makeTableData(book.author));
+  tr.appendChild(makeTableData(book.numPages));
+  tr.appendChild(makeTableData(book.isRead));
+  return tr;
+}
+
+function showBooks(){
+  const tableRowList = document.querySelector('table').children[0].children;
+  while(tableRowList.length > 1){  //emptying table
+    tableRowList[1].remove();
+  }
+  for(let i=0; i<myLibrary.length; ++i){
+    document.querySelector('table').appendChild(makeRow(myLibrary[i]));
+  }
+}
+
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
